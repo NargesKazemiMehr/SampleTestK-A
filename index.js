a1d757b3d7b52c2480c1550ba7d9d000417b4a71
Narges <50104356+NargesKazemiMehr@users.noreply.github.com>
2021-04-11 13:49:28 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+$( document ).ready(function() {
+ AnimateRotate(360);
+});
+function AnimateRotate(d){
+    var elem = $("#Box");
+
+    $({deg: 0}).animate({deg: d}, {
+        duration: 2000,
+        step: function(now){
+            elem.css({
+                 transform: "rotate(" + now + "deg)"
+            });
+        }
+		
+    });
+}
